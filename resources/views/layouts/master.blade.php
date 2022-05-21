71334cfd97d5361955719be635b9680535556400
Lokesh <sharmalk1991@gmail.com>
2022-05-21 21:01:55 +0530

    Login page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,7 +43,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <router-link to="/dashboard" class="brand-link">
-      <img src="{{ asset('/images/logo.png') }}" alt="The Logo" class="brand-image img-circle elevation-3"
+      <img src="{{ asset('/images/logo-admin.png') }}" alt="The Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
     </router-link>
